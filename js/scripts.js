847d33f5fc97d111a91de4902232689c1fdd0eea
Rory <rorymartin98@gmail.com>
2018-07-13 15:00:39 +0930

    up 3 hoursish

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,25 @@
 
 <!--
 
-/* ----------- Desc ------------- */
+    function scrollFunction() {
+        window.onbeforeunload = function () {
+        window.scrollTo(0,0);
+        
+        
+        window.onscroll = function() {
+            headerFloat();
+        }
+        
+        var header = document.getElementById("h_Container");
+        var sticky = header.offsetTop;
 
-    /*-- Global Variables */
-            
-    /*-- Intiate Function. */
-            function intiate() {
+        function headerFloat() {
+            if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
             }
+            else {
+                header.classList.remove("sticky");
+            }
+        }
+    }
 -->
